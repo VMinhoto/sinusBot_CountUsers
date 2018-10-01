a8f156bae3a2b7be58e5f3f75a0f4f3cdc547def
VMinhoto <vitor.v.minhoto@gmail.com>
2018-10-01 12:12:34 +0100

    Added some comments

--- UsersCounter.js
+++ UsersCounter.js
@@ -4,6 +4,10 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
+
+/*
+    This 1st block of code defines the guy in sinusbot webpage
+*/
 registerPlugin({
     name: 'OnlineUsersCounter',
     version: '1.0.0',
@@ -110,7 +114,9 @@ registerPlugin({
 
      
 
-
+    /*
+     This functions count online admins
+    */
      function adminsOnline(){
          var clients = backend.getClients();
          var admins = 0;
